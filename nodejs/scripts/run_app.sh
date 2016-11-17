3d7665cc6ba8777ed9ba73f65153c30387f05ab6
romejiang <romejiang@gmail.com>
2016-11-18 04:17:08 +0800

    in the shell add run log

--- nodejs/scripts/run_app.sh
+++ nodejs/scripts/run_app.sh
@@ -1,9 +1,11 @@
 set -e
-
+echo 'Start Meteorup container.'
 if [ -d /bundle ]; then
+  echo 'Extract bundle file.'
   cd /bundle
   tar xzf *.tar.gz
 elif [[ $BUNDLE_URL ]]; then
+  echo 'Extract internet bundle.'
   cd /tmp
   curl -L -o bundle.tar.gz $BUNDLE_URL
   tar xzf bundle.tar.gz
@@ -11,27 +13,59 @@ else
   echo "=> You don't have an meteor app to run in this image."
   exit 1
 fi
-
+echo 'Detection of nodejs version'
 [ -s "$HOME/.nvm/nvm.sh" ] && . "$HOME/.nvm/nvm.sh"
-METEOR_VERSION=`grep meteorRelease bundle/star.json | awk '{print $2}'|sed -e 's/\"METEOR@\(.*\)\"/\1/g'`
-if [ "$METEOR_VERSION"x = "1.4"x ];then
+METEOR_VERSION=`grep meteorRelease bundle/star.json | awk '{print $2}'|sed -e 's/\"METEOR@\(.*\)\"/\1/g'|awk -F'.' '{print $2}'`
+if [ "$METEOR_VERSION"x = "4"x ];then
   nvm use 4.4.7
-elif [ "$METEOR_VERSION"x = "1.3"x ]; then
+elif [ "$METEOR_VERSION"x = "3"x ]; then
   nvm use 0.10.41
-elif [ "$METEOR_VERSION"x = "1.2"x ]; then
+elif [ "$METEOR_VERSION"x = "2"x ]; then
   nvm use 0.10.40
-elif [ "$METEOR_VERSION"x = "1.0"x ]; then
+elif [ "$METEOR_VERSION"x = "0"x ]; then
   nvm use 0.10.33
 fi
 
+if [[ $NODE_VERSION ]]; then
+  echo 'Install node version to $NODE_VERSION'
+  nvm install $NODE_VERSION
+  nvm use $NODE_VERSION
+fi
+
+grep meteorRelease bundle/star.json | awk '{print $2}'|sed -e 's/\"METEOR@\(.*\)\"/\1/g' 
+node -v
+
 cd bundle/programs/server/
+
+if [[ ! $SHRINKWRAP ]]; then
+  echo 'process npm-shrinkwrap.json'
+  sed -i 's/"resolved.*,/"!!":"",/g' npm-shrinkwrap.json
+  sed -i 's/"resolved.*"/"!!":""/g' npm-shrinkwrap.json
+fi
+
 shopt -s expand_aliases
-alias cnpm="npm --registry=https://registry.npm.taobao.org --cache=$HOME/.npm/.cache/cnpm --disturl=https://npm.taobao.org/dist --userconfig=$HOME/.cnpmrc"
-cnpm i
+echo 'npm install.'
+if [[ $NPM_DEFAULT ]]; then
+  if [[ $NPM_DEBUG ]]; then
+    alias cnpm="npm --loglevel verbose"
+  else
+    alias cnpm="npm"
+  fi
+else
+  if [[ $NPM_DEBUG ]]; then
+    alias cnpm="npm --loglevel verbose --registry=https://registry.npm.taobao.org --cache=$HOME/.npm/.cache/cnpm --disturl=https://npm.taobao.org/dist --userconfig=$HOME/.cnpmrc"
+  else
+    alias cnpm="npm --registry=https://registry.npm.taobao.org --cache=$HOME/.npm/.cache/cnpm --disturl=https://npm.taobao.org/dist --userconfig=$HOME/.cnpmrc"
+  fi
+fi
+
+cnpm install
+echo 'npm install complete'
 cd ../../
 
 if [[ $REBUILD_NPM_MODULES ]]; then
   if [ -f /opt/meteord/rebuild_npm_modules.sh ]; then
+    echo 'Rebuild npm modules'
     cnpm install -g node-gyp
     cd programs/server
     bash /opt/meteord/rebuild_npm_modules.sh
@@ -50,7 +84,6 @@ fi
 
 # Honour already existing PORT setup
 export PORT=${PORT:-80}
-node -v
 echo "=> Starting meteor app on port:$PORT"
 node main.js
 
