9e4d4c4c5860759d609d160ddb55d508ed7f3cd6
romejiang <romejiang@gmail.com>
2016-11-18 04:11:31 +0800

    update yum source to aliyun & 163

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 set -e
+cat << EOF >/etc/apt/sources.list 
+deb http://mirrors.163.com/debian/ wheezy main non-free contrib
+deb http://mirrors.163.com/debian/ wheezy-updates main non-free contrib
+deb http://mirrors.163.com/debian/ wheezy-backports main non-free contrib
+deb-src http://mirrors.163.com/debian/ wheezy main non-free contrib
+deb-src http://mirrors.163.com/debian/ wheezy-updates main non-free contrib
+deb-src http://mirrors.163.com/debian/ wheezy-backports main non-free contrib
+deb http://mirrors.163.com/debian-security/ wheezy/updates main non-free contrib
+deb-src http://mirrors.163.com/debian-security/ wheezy/updates main non-free contrib 
+deb http://mirrors.aliyuncs.com/debian/ wheezy main non-free contrib
+deb http://mirrors.aliyuncs.com/debian/ wheezy-proposed-updates main non-free contrib
+deb-src http://mirrors.aliyuncs.com/debian/ wheezy main non-free contrib
+deb-src http://mirrors.aliyuncs.com/debian/ wheezy-proposed-updates main non-free contrib
+EOF
 apt-get update -y
-apt-get install -y libfreetype6 libfreetype6-dev fontconfig curl bzip2 build-essential python git libssl-dev
+apt-get install -y libfreetype6 libfreetype6-dev fontconfig curl bzip2 build-essential python git libssl-dev imagemagick graphicsmagick
+
